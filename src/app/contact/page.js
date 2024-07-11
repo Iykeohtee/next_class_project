beb5f932a0fcede159cef5bbdcffb02c54643040
Ajiri <dimaroaj@gmail.com>
2024-07-11 12:29:48 +0100

    edited work for login component

---
+++ src/app/contact/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const contact = () => {
+  return (
+    <div>
+      Contact Us
+    </div>
+  )
+}
+
+export default contact
---
+++ src/app/dashboard/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './dashboard.css';
+
+const dashboard = () => {
+  return (
+    <div>
+      Welcome Dimaro
+      <p className='hfh'>we inspire elegance while giving you serene comfort luxurious style</p>
+    </div>
+  )
+}
+
+export default dashboard
