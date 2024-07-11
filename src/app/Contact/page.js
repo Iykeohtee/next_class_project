1b40031bed79bb160666e3dd6a661586ef3e28db
Amachineche <amaphilosophy@gmail.com>
2024-07-11 12:19:41 +0100

    nextjs and react learning

---
+++ src/app/Contact/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const contact = () => {
+  return (
+    <div>
+      our contacts
+    </div>
+  )
+}
+
+export default contact
---
+++ src/app/Dashboard/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './dashboard.css'
+
+const Dashboard = () => {
+  return (
+    <div>
+      Our Dashboard
+      <p className='p'> I am Chineche</p>
+    </div>
+  )
+}
+
+export default Dashboard
