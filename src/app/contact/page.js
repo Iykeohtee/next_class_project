a08c7704845b20186b0f5e559969654443a50800
GodwinAtteh <geeciatteh@gmail.com>
2024-07-11 12:31:05 +0100

    first commit

---
+++ src/app/contact/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const contact = () => {
+  return (
+    <div>Contact Us</div>
+  )
+}
+
+export default contact
---
+++ src/component/Loading.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const loading = () => {
+  return (
+    <div> 
+      <p>loading...</p> 
+    </div>
+  )
+}
+
+export default loading
