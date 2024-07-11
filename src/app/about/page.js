83a604bad70a73b611e6017e4ab6080704a43510
Kelechi Faith <kelechif4@gmail.com>
2024-07-11 12:21:29 +0100

    Started with next.js today

---
+++ src/app/about/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const about = () => {
+  return (
+    <div>About Us</div>
+  )
+}
+
+export default about;
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import styles from "./dashboard.css,";
 
 export default function Home() {
   return (
     <main>
       Home
+      <Loading/>
     </main>
   );
 }
