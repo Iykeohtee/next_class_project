754e7dcb0b0dc62f28d39ab8a210f9bde1ed0807
fubs <imaerele.fubara@gmail.com>
2024-07-11 12:53:53 +0100

    work on component

---
+++ src/app/Dashboard/page.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './dashboard.module.css';
+
+const dasdboard = () => {
+  return (
+    <div>
+      My Dashboard 
+      <p className='p'>loooooo gggg hhhh</p>
+    </div>
+  )
+}
+
+export default dasdboard
---
+++ src/components/Loading.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <div>
+      <p>loading...</p>
+    </div>
+  )
+}
+
+export default Loading;
