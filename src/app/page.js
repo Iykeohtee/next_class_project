f3ccbb41ea7e726e131aa7115a3345ab6d11ceb9
samsom <samsonrely1000@gmail.com>
2024-07-16 12:23:40 +0100

    props

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,19 @@
+"use client"
 import Image from "next/image";
 import Loading from "@/components/Loading";
 
 export default function Home() {
+  function getData(){
+    const res = fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then(response => response.json())
+    .then(json => console.log(json))
+  }
+  getData();
 
   return (
-    <main>
+    <div>
     
     <Loading/> 
-    </main>
+    </div>
   );
 }
