594129f2df46cea67f2aaa9ec0b3415706f6d703
Estheticallybawo <estheticallybawo@gmail.com>
2024-07-19 21:33:26 +0100

    Problems Issue Resolved

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -5,7 +5,7 @@ import Link from "next/link"
 
 const Navbar = () => {
   return (
-      <nav className="bg-black">
+      <nav className="bg-white drop-shadow-2xl">
         <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>    
           <div className='flex items-center justify-between h-16'>
             <div className='flex items-center'>
@@ -15,10 +15,10 @@ const Navbar = () => {
         </div>
         <div className='hidden md:block'>
           <div className='ml-4 flex items-center space-x-4'>
-            <Link href='/' className='text-white hover:bg-white hover:text-black rounded-lg p-2'>Home</Link>
-            <Link href='/' className='text-white hover:bg-white hover:text-black rounded-lg p-2'>About</Link>
-            <Link href='/' className='text-white hover:bg-white hover:text-black rounded-lg p-2'>Services</Link>
-            <Link href='/' className='text-white hover:bg-white hover:text-black rounded-lg p-2'>Contact</Link>
+            <Link href='/' className='text-red-500 hover:bg-black hover:text-white rounded-lg p-2'>Home</Link>
+            <Link href='/' className='text-red-500 hover:bg-black hover:text-white rounded-lg p-2'>About</Link>
+            <Link href='/' className='text-red-500 hover:bg-black hover:text-white rounded-lg p-2'>Services</Link>
+            <Link href='/' className='text-red-500 hover:bg-black hover:text-white rounded-lg p-2'>Contact</Link>
           </div>
         </div>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,5 +3,6 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: []
+    }
+ 
