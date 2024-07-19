e83df4077ebbd50f2df2236260e547845807ae80
Estheticallybawo <estheticallybawo@gmail.com>
2024-07-19 23:35:40 +0100

    Search Bar Added

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -44,36 +44,6 @@ const Navbar = () => {
             <Link href='/' className='text-red-500 hover:bg-black hover:text-white rounded-lg p-2'>Contact</Link>
           </div>
         </div>
-      <div className="md:hidden flex items-center">
-        <button className="inline-flex items-center justify-center p-2 rounded-md text-red-500 hover:text-black focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-        onClick={toggleNavbar}>
-        {
-          isClick ? (<svg  className="h-6 w-6"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor" >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M6 18L18 6M6 6l12 12"/>
-</svg>) : (
-          <svg  className="h-6 w-6"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor">
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M4 6h16M4 12h16m-7 6h7"/>
-</svg>)
-        }
-            
-                  </button>
-        </div>
       </div>
       </div>
   </nav>
